fd7423549ee60270b7789230f8d7f358c77a4d47
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-04-29 02:50:03 +0200

    31-creating array of scraping objects

--- scrap software jobs using puppeteer/index.js
+++ scrap software jobs using puppeteer/index.js
@@ -1,11 +1,32 @@
+const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
-
+const scrapingResults = [
+  {
+    title: "Entry Level Software Engineer - C or C++",
+    datePosted: new Date("2019-07-26 12:00:00"),
+    neighborhood: "(palo alto)",
+    url: "https://sfbay.craigslist.org/pen/sof/d/palo-alto-entry-level-software-engineer/6943135190.html",
+    jobDescription:
+      "Major Technology company is seeking an Entry Level software Engineer. The ideal candidate will have extensive school project experience with C or C++. Under general supervision...",
+    compensation: "Up to US$0.00 per year",
+  },
+];
 async function main() {
   const browser = await puppeteer.launch({
     headless: false,
   });
   const page = await browser.newPage();
-  await page.goto("https://www.google.com/");
+  await page.goto("https://sfbay.craigslist.org/search/sof");
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  const results = $(".result-title")
+    .map((index, element) => {
+      const title = $(element).text();
+      const url = $(element).attr("href");
+      return { title, url };
+    })
+    .get();
+  console.log(results);
 }
 
 main();
